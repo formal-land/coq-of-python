1451c0acc430e46356d6e30a67f87f478175d2e7
Guillaume Claret <dev@clarus.me>
2024-05-06 20:36:01 +0200

    Some translation for most of the AST

--- src/ethereum/paris/vm/instructions/arithmetic.py
+++ src/ethereum/paris/vm/instructions/arithmetic.py
@@ -27,7 +27,7 @@ def generate_mod(node):
     elif isinstance(node, ast.Interactive):
         return generate_error(node)
     else:
-        assert False
+        return generate_error(node)
 
 
 def generate_stmt(node):
@@ -50,7 +50,8 @@ def generate_stmt(node):
     #     return generate_error(node)
     elif isinstance(node, ast.AugAssign):
         return "  let " + generate_expr(node.target) + " := " + \
-            generate_expr(node.op) + " " + generate_expr(node.value) + " in"
+            generate_operator(node.op) + " " + \
+            generate_expr(node.value) + " in"
     elif isinstance(node, ast.AnnAssign):
         return generate_error(node)
     elif isinstance(node, ast.For):
@@ -106,28 +107,151 @@ def generate_stmt(node):
     elif isinstance(node, ast.Continue):
         return "  Continue."
     else:
-        assert False
-
-
-def generate_arg(arg):
-    return "(" + arg.arg + " : " + arg.annotation.id + ")"
+        return generate_error(node)
 
 
 def generate_expr(node):
-    if isinstance(node, ast.BinOp):
-        return "(" + generate_expr(node.op) + \
+    if isinstance(node, ast.BoolOp):
+        return "(" + generate_boolop(node.op) + \
+            " ".join(generate_expr(value) for value in node.values) + ")"
+    elif isinstance(node, ast.NamedExpr):
+        return "(" + generate_expr(node.target) + " := " + \
+            generate_expr(node.value) + ")"
+    elif isinstance(node, ast.BinOp):
+        return "(" + generate_operator(node.op) + \
             generate_expr(node.left) + " " + generate_expr(node.right) + ")"
+    elif isinstance(node, ast.UnaryOp):
+        return "(" + generate_unaryop(node.op) + generate_expr(node.operand) + ")"
+    elif isinstance(node, ast.Lambda):
+        return f"fun {generate_expr(node.args)} => {generate_expr(node.body)}"
+    elif isinstance(node, ast.IfExp):
+        return f"If {generate_expr(node.test)} then {generate_expr(node.body)} else {generate_expr(node.orelse)}"
+    elif isinstance(node, ast.Dict):
+        return "{" + ", ".join(generate_expr(key) + ": " + generate_expr(value) for key, value in zip(node.keys, node.values)) + "}"
+    elif isinstance(node, ast.Set):
+        return "{" + ", ".join(generate_expr(elt) for elt in node.elts) + "}"
+    elif isinstance(node, ast.ListComp):
+        return f"[{generate_expr(node.elt)} for {generate_expr(node.generators)}]"
+    elif isinstance(node, ast.SetComp):
+        return f"{{{generate_expr(node.elt)} for {generate_expr(node.generators)}}}"
+    elif isinstance(node, ast.DictComp):
+        return f"{{{generate_expr(node.key)}: {generate_expr(node.value)} for {generate_expr(node.generators)}}}"
+    elif isinstance(node, ast.GeneratorExp):
+        return f"({generate_expr(node.elt)} for {generate_expr(node.generators)})"
+    elif isinstance(node, ast.Await):
+        return f"Await {generate_expr(node.value)}"
+    elif isinstance(node, ast.Yield):
+        return f"Yield {generate_expr(node.value)}"
+    elif isinstance(node, ast.YieldFrom):
+        return f"YieldFrom {generate_expr(node.value)}"
+    elif isinstance(node, ast.Compare):
+        return f"{generate_expr(node.left)} {generate_cmpop(node.ops[0])} {generate_expr(node.comparators[0])}"
+    elif isinstance(node, ast.Call):
+        return f"{generate_expr(node.func)}({', '.join(generate_expr(arg) for arg in node.args)})"
+    elif isinstance(node, ast.FormattedValue):
+        return f"{generate_expr(node.value)}"
+    elif isinstance(node, ast.JoinedStr):
+        return f"{''.join(generate_expr(value) for value in node.values)}"
     elif isinstance(node, ast.Constant):
         return str(node.value)
-    elif isinstance(node, ast.Call):
-        return generate_expr(node.func) + " " + \
-            " ".join(generate_expr(arg) for arg in node.args)
+    elif isinstance(node, ast.Attribute):
+        return f"{generate_expr(node.value)}.{node.attr}"
+    elif isinstance(node, ast.Subscript):
+        return f"{generate_expr(node.value)}[{generate_expr(node.slice)}]"
+    elif isinstance(node, ast.Starred):
+        return f"*{generate_expr(node.value)}"
     elif isinstance(node, ast.Name):
         return node.id
-    elif isinstance(node, ast.Attribute):
-        return generate_expr(node.value) + "." + node.attr
+    elif isinstance(node, ast.List):
+        return f"[{', '.join(generate_expr(elt) for elt in node.elts)}]"
+    elif isinstance(node, ast.Tuple):
+        return f"({', '.join(generate_expr(elt) for elt in node.elts)})"
+    elif isinstance(node, ast.Slice):
+        return f"{generate_expr(node.lower)}:{generate_expr(node.upper)}"
     else:
-        return f"(* Unsupported expr node type: {type(node).__name__} *)"
+        return generate_error(node)
+
+
+def generate_boolop(node):
+    if isinstance(node, ast.And):
+        return "And"
+    elif isinstance(node, ast.Or):
+        return "Or"
+    else:
+        return generate_error(node)
+
+
+def generate_operator(node):
+    if isinstance(node, ast.Add):
+        return "+"
+    elif isinstance(node, ast.Sub):
+        return "-"
+    elif isinstance(node, ast.Mult):
+        return "*"
+    elif isinstance(node, ast.MatMult):
+        return "@"
+    elif isinstance(node, ast.Div):
+        return "/"
+    elif isinstance(node, ast.Mod):
+        return "%"
+    elif isinstance(node, ast.Pow):
+        return "**"
+    elif isinstance(node, ast.LShift):
+        return "<<"
+    elif isinstance(node, ast.RShift):
+        return ">>"
+    elif isinstance(node, ast.BitOr):
+        return "|"
+    elif isinstance(node, ast.BitXor):
+        return "^"
+    elif isinstance(node, ast.BitAnd):
+        return "&"
+    elif isinstance(node, ast.FloorDiv):
+        return "//"
+    else:
+        return generate_error(node)
+
+
+def generate_unaryop(node):
+    if isinstance(node, ast.Invert):
+        return "~"
+    elif isinstance(node, ast.Not):
+        return "!"
+    elif isinstance(node, ast.UAdd):
+        return "+"
+    elif isinstance(node, ast.USub):
+        return "-"
+    else:
+        return generate_error(node)
+
+
+def generate_cmpop(node):
+    if isinstance(node, ast.Eq):
+        return "=="
+    elif isinstance(node, ast.NotEq):
+        return "!="
+    elif isinstance(node, ast.Lt):
+        return "<"
+    elif isinstance(node, ast.LtE):
+        return "<="
+    elif isinstance(node, ast.Gt):
+        return ">"
+    elif isinstance(node, ast.GtE):
+        return ">="
+    elif isinstance(node, ast.Is):
+        return "Is"
+    elif isinstance(node, ast.IsNot):
+        return "IsNot"
+    elif isinstance(node, ast.In):
+        return "In"
+    elif isinstance(node, ast.NotIn):
+        return "NotIn"
+    else:
+        return generate_error(node)
+
+
+def generate_arg(node):
+    return "(" + node.arg + " : " + node.annotation.id + ")"
 
 
 file_name = "../execution-specs/src/ethereum/paris/vm/instructions/arithmetic.py"
