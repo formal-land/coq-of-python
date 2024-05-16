577d9a2e0a78a555d7e65b7188526ddbcbf656e7
Guillaume Claret <dev@clarus.me>
2024-05-16 19:11:15 +0200

    Test: compute of the add function

--- CoqOfPython/ethereum/paris/vm/instructions/simulations/arithmetic.v
+++ CoqOfPython/ethereum/paris/vm/instructions/simulations/arithmetic.v
@@ -31,7 +31,7 @@ Definition add : MS? Evm.t Exception.t unit :=
   letS? y := StateError.lift_lens Evm.Lens.stack pop in
 
   (* GAS *)
-  letS? _ := charge_gas GAS_VERY_LOW in
+  (* letS? _ := charge_gas GAS_VERY_LOW in *)
 
   (* OPERATION *)
   let result := U256.wrapping_add x y in
@@ -43,3 +43,40 @@ Definition add : MS? Evm.t Exception.t unit :=
     (inl tt, Uint.__add__ pc (Uint.Make 1))) in
 
   returnS? tt.
+
+Axiom environment : __init__.Environment.t.
+Axiom message : __init__.Message.t Evm.t.
+
+Definition empty_evm : Evm.t :=
+  Evm.Make message {|
+    Evm.Rest.pc := Uint.Make 0;
+    Evm.Rest.stack := [];
+    Evm.Rest.memory := [];
+    Evm.Rest.code := __init__.Bytes.Make [];
+    Evm.Rest.gas_left := Uint.Make 0;
+    Evm.Rest.env := environment;
+    Evm.Rest.valid_jump_destinations := [];
+    Evm.Rest.logs := [];
+    Evm.Rest.refund_counter := 0;
+    Evm.Rest.running := true;
+    Evm.Rest.output := __init__.Bytes.Make [];
+    Evm.Rest.accounts_to_delete := [];
+    Evm.Rest.touched_accounts := [];
+    Evm.Rest.return_data := __init__.Bytes.Make [];
+    Evm.Rest.error := None;
+    Evm.Rest.accessed_addresses := [];
+    Evm.Rest.accessed_storage_keys := [];
+  |}.
+
+Definition evm_with_gas : Evm.t :=
+  Evm.Lens.gas_left.(Lens.write) empty_evm GAS_VERY_LOW.
+
+Definition evm_with_stack : Evm.t :=
+  Evm.Lens.stack.(Lens.write) evm_with_gas [
+    U256.of_Z 12;
+    U256.of_Z 23
+  ].
+
+(* The term does not fully evaluate as the `charge_gas` function is still an axiom, so we have to
+   comment its call in the code above. *)
+Compute (add evm_with_stack).
