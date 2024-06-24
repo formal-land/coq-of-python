7bbc8b2fed704b9296be14574f1f62dbe7ce594e
InfiniteEchoes <128528395+InfiniteEchoes@users.noreply.github.com>
2024-06-24 15:00:04 +0800

    Minor progress on `evm`

--- CoqOfPython/ethereum/paris/vm/instructions/simulations/storage.v
+++ CoqOfPython/ethereum/paris/vm/instructions/simulations/storage.v
@@ -15,6 +15,7 @@ Module Evm := __init__.Evm.
 Module Message := __init__.Message.
 Module State := __init__.State.
 Module Address := __init__.Address.
+Module Environment := __init__.Environment.
 
 Require ethereum.paris.vm.simulations.gas.
 Definition GAS_COLD_SLOAD := gas.GAS_COLD_SLOAD.
@@ -36,8 +37,9 @@ Definition get_storage (state : State.t) (address : Address.t) (key : Bytes.t) :
 (* def get_storage_original(state: State, address: Address, key: Bytes) -> U256: *)
 Definition get_storage_original (state : State.t) (address : Address.t) (key : Bytes.t) : U256.t. Admitted.
 
-(* TODO: (progress) things to test on this draft:
+(* TODO: (progress) things to do on this draft:
 - test how to update a value for evm
+- replace `List.In` with `List.find` + `is_none`(?)
 - rest of the draft
 *)
 
@@ -88,19 +90,31 @@ Definition sload : MS? Evm.t Exception.t unit :=
     let '(Evm.Make message rest) := evm in
     let evm_message_current_target := message.(Message.current_target) in
     let evm_rest_accessed_storage_keys := rest.(Evm.Rest.accessed_storage_keys) in
-    (* NOTE: accessed_storage_keys.add is actually `pair.add` and we simulate it directly *)
-
     (* Example code for value update:
       | Address.evm => fun v => Some (h <| evm := v |>)
       | Address.stack => fun v => Some (h <| stack := v |>)
-    *)
 
+      
+    Definition pc : Lens.t t Uint.t := {|
+      Lens.read '(Make _ rest) := rest.(Rest.pc);
+      Lens.write '(Make message rest) pc := Make message rest<|Rest.pc := pc|>;
+    |}.
+    *)
+    (* NOTE: accessed_storage_keys.add is actually `pair.add` and we simulate it directly *)
     letS? _ := 
     if List.In (evm_message_current_target, key) evm_rest_accessed_storage_keys
     then charge_gas GAS_WARM_ACCESS
-    else (letS? _ := 
-      (fun v => evm_rest_accessed_storage_keys <| (evm.message.current_target, key) |>) (* ?????? *)
-    (* evm.accessed_storage_keys.add((evm.message.current_target, key)) *) in
+    else (letS? _ := unit
+    (* TODO: Correctly write a state-update code *)
+    (* evm.accessed_storage_keys.add((evm.message.current_target, key)) *)
+    (* (fun v => evm_rest_accessed_storage_keys <| (evm.message.current_target, key) |>)  *)
+    (* 
+    TODO:
+    1. correctly construct the updated value
+    2. stuff the value into the evm correctly
+    3. update the evm using writeS?
+    *)
+    in
       charge_gas GAS_COLD_SLOAD)
     in
     (* OPERATION *)
